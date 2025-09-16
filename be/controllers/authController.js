10b1e7679db95cdedec8b3b3ce538c302246fd07
Mohammad Kaif Ali Ansari <150768264+mkali00001@users.noreply.github.com>
2025-09-16 14:53:14 +0530

    clg

--- be/controllers/authController.js
+++ be/controllers/authController.js
@@ -20,6 +20,7 @@ const signup = async (req, res) => {
 
     // --- CAPTCHA verification (cookie-based) ---
     const storedCaptcha = req.cookies?.captcha_text;
+    console.log(storedCaptcha)
     if (!storedCaptcha) {
       return res.status(400).json({ message: "Captcha missing or expired" });
     }

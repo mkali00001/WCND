2be89295ee0d378ac39893d441657d7fb1b059e0
Mohammad Kaif Ali Ansari <150768264+mkali00001@users.noreply.github.com>
2025-09-25 21:15:41 +0530

    ppp

--- be/routes/payment.route.js
+++ be/routes/payment.route.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const paymentRouter = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const {
-  createPayment,
-  getPayments,
-  createOrder,
-  recordPayment,
-  getPaymentCategorie,
-  getMyPayment,
-  generateInvoice,
-  downloadInvoice,
+const { 
+    createPayment, 
+    getPayments, 
+    createOrder, 
+    recordPayment,
+    getPaymentCategorie,
+    getMyPayment,
+    generateInvoice,
+    downloadInvoice
 } = require('../controllers/payment.controller');
-
 
 paymentRouter.use(authMiddleware);
 
+paymentRouter.post("/create-order", createOrder)
+paymentRouter.post("/record-payment", recordPayment)
 
-paymentRouter.post('/create-order', createOrder);
-paymentRouter.post('/record-payment', recordPayment);
-paymentRouter.post('/create-payment', createPayment);
-paymentRouter.get('/get-payment', getPayments);
+paymentRouter.post('/create-payment',  createPayment)
+paymentRouter.get('/get-payment',  getPayments)
+paymentRouter.post("/paymentcategory",getPaymentCategorie)
 paymentRouter.get('/my-payment', getMyPayment);
-paymentRouter.post('/invoice', generateInvoice);
-paymentRouter.get('/download-invoice', downloadInvoice);
+paymentRouter.post("/invoice", generateInvoice)
+paymentRouter.get("/download-invoice", downloadInvoice)
+
 
 module.exports = paymentRouter;

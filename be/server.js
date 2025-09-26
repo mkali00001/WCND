d54ddceae4469f18b00fb4488a0ef93321c9e0ef
SEIntern <intern.softwareengineer.tryidol@gmail.com>
2025-09-26 12:56:49 +0530

    Update server.js

--- be/server.js
+++ be/server.js
@@ -7,10 +7,10 @@ connectDB();
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../fe/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../fe", "dist", "index.html"));
   });
 }
 
